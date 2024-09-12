3c71b35c74c25db450440d352bd62f227d369d56
EranTimothy-dev <eranperera.dev@gmail.com>
2024-09-12 21:21:40 +0530

    fixed bug in solution

--- TwoPointers/SquaresofaSortedArray.java
+++ TwoPointers/SquaresofaSortedArray.java
@@ -7,15 +7,17 @@ public class SquaresofaSortedArray {
         int left = 0;
         int right = nums.length -1 ;
         int[] result = new int[nums.length];
-        int index= nums.length -1;
+        int index = nums.length -1;
 
-        while (left < right){
-            if (Math.abs(nums[left]) < Math.abs(nums[right])){
-                result[index--] = nums[left] * nums[left];
+        while (left <= right){
+            if (Math.abs(nums[left]) > Math.abs(nums[right])){
+                result[index--] = (int) Math.pow(nums[left], 2);
                 left++;
+                // index--;
             } else {
                 result[index--] = nums[right] * nums[right];
                 right--;
+                // index--;
             }
         }
         return result;
@@ -24,7 +26,11 @@ public class SquaresofaSortedArray {
     public static void main(String[] args){
 
         int[] nums = {-4,-1,0,3,10};
-        System.out.println(sortedSquares(nums));
+        int[] result = sortedSquares(nums);
+        // System.out.println(result.toString());
+        for (int i: result){
+            System.out.println(i);
+        }
     } 
 }
 
