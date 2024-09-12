56ca4f126b5d15f480dbd615012e51140d4dc286
EranTimothy-dev <eranperera.dev@gmail.com>
2024-09-12 16:31:36 +0530

    added sorted squares for java

--- TwoPointers/SquaresofaSortedArray.java
+++ TwoPointers/SquaresofaSortedArray.java
@@ -1,5 +1,30 @@
 package TwoPointers;
 
 public class SquaresofaSortedArray {
-    
+
+    public static int[]sortedSquares(int[] nums){
+
+        int left = 0;
+        int right = nums.length -1 ;
+        int[] result = new int[nums.length];
+        int index= nums.length -1;
+
+        while (left < right){
+            if (Math.abs(nums[left]) < Math.abs(nums[right])){
+                result[index--] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+
+    }
+    public static void main(String[] args){
+
+        int[] nums = {-4,-1,0,3,10};
+        System.out.println(sortedSquares(nums));
+    } 
 }
+
