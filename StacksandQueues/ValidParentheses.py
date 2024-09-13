a6f540192847dbc0ba8d2510c40e93d7f08cd55d
EranTimothy-dev <eranperera.dev@gmail.com>
2024-09-13 14:11:43 +0530

    added space optimised soulution for valid parentheses python question

--- StacksandQueues/ValidParentheses.py
+++ StacksandQueues/ValidParentheses.py
@@ -12,6 +12,19 @@ class Solution:
 
         return not stack
     
+    def isValid2(self, s:str) -> bool:
+        stack = []
+        for c in s:
+            if c in '([{':
+                stack.append(c)
+            else:
+                if not stack or \
+                    c == ')' and stack[-1] != '(' or \
+                    c == ']' and stack[-1] != '[' or \
+                    c == '}' and stack[-1] != '{':
+                    return False
+                stack.pop()
+        return not stack
     
     
 if __name__ == "__main__":
