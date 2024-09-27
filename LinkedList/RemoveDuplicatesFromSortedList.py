4c20b2325c6f31207387821c7338b4934aded07d
EranTimothy-dev <eranperera.dev@gmail.com>
2024-09-27 23:51:39 +0530

    fixed type annotation

--- LinkedList/RemoveDuplicatesFromSortedList.py
+++ LinkedList/RemoveDuplicatesFromSortedList.py
@@ -1,10 +1,13 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+        
+        
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
         cur = head
         while cur and cur.next:
             if cur.val == cur.next.val:
