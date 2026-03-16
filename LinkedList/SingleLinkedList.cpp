896438ba0a7b9da733ea21b5866a5555d51a8126
EranTimothy-dev <eranperera.dev@gmail.com>
2026-03-16 14:47:01 +0530

    implemented singly linked list struct and both iterative and recursive find methods

---
+++ LinkedList/SingleLinkedList.cpp
@@ -0,0 +1,35 @@
+struct linkedListNode {
+    int data;
+    linkedListNode* next;
+
+    linkedListNode(int value) : data(value), next(nullptr) {}
+
+    void setNext(linkedListNode* nextNode) {
+        next = nextNode;
+    }
+};
+
+linkedListNode* head = nullptr;
+
+int find(int value) {
+    linkedListNode* current = head;
+    while (current != nullptr) {
+        if (current->data == value) {
+            return current->data;
+        } else {
+            current = current->next;
+        }
+    }
+    return -1;
+}
+
+int findRecursive(linkedListNode* current, int value) {
+    // linkedListNode* current = head;
+    if (current == nullptr) {
+        return -1;
+    } else if (current->data == value) {
+        return current->data;
+    } else {
+        return findRecursive(current->next, value);
+    }
+}
